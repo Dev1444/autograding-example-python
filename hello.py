42b73d8026264eb6edacbb37ce686e41cf055dae
Dev1444 <98470656+Dev1444@users.noreply.github.com>
2022-02-17 11:48:11 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,20 +1,34 @@
-# NAME:         FIXME
+# NAME:         Deven Mistry
 # ASSIGNMENT:   Project 1
 
-# Example
-def hello_world():
-    return "Hello!"
+# Problem 1: squared_sum
+def squared_sum(squared):
+    squaredsum = 0
+    for element in squared: # Reading list
+        squaredsum += element ** 2 # Finding sum
+    return squaredsum # Finds result
 
-# 1
-def squared_sum(array):
-    return 0
+if __name__ == '__main__': 
+    print(squared_sum([])) # Running test cases 
+    print(squared_sum([5,-2,3]))
+    print(squared_sum([-3,4]))
+    print(squared_sum([7,-1,15,0]))
 
-# 2
-def mix(a, b):
-    return ""
+print('')
 
-def main():
-    print("squared sum: ", squared_sum([-3, 4]))
-    print("mix:         ", mix("1234", "abcd") == "1a2b3c4d")
-
-main()
+# Problem 2: mix
+def mix(s1,s2): 
+    case = ""
+    for x in range(0,min(len(s1),len(s2))): # Calculating length 
+        case = case + s1[x] + s2[x]
+        
+    if(len(s1)>len(s2)): 
+        case = case + s1[x+1:]
+    else:
+        case = case + s2[x+1:]
+    print(case)
+    # Test cases
+mix("hello", "there")    
+mix("1234", "abcd")
+mix("12", "abcd")
+mix("1234", "ab")
